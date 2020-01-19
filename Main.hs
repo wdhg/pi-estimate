3edea98c68376d334f40e091f2dfc4d75704c205
wdhg <wdhgrant@gmail.com>
2020-01-19 22:58:48 +0000

    initial commit

---
+++ Main.hs
@@ -0,0 +1,35 @@
+import System.Random
+
+data Coord
+  = Coord Double Double
+    deriving
+      Show
+
+instance Bounded Coord where
+  minBound = Coord 0.0 0.0
+  maxBound = Coord 1.0 1.0
+
+instance Random Coord where
+  randomR (Coord lowerX lowerY, Coord upperX upperY) g
+    = (Coord x y, g'')
+      where
+        (x, g')  = randomR (lowerX, upperX) g
+        (y, g'') = randomR (lowerY, upperY) g'
+  random g
+    = randomR (minBound, maxBound) g
+
+magnitude :: Coord -> Double
+magnitude (Coord x y)
+  = sqrt $ (x ^ 2) + (y ^ 2)
+
+estimate :: Int -> IO ()
+estimate n
+  = do
+    g <- newStdGen
+    let points = take n $ randoms g
+        count = length $ filter ((< 1.0) . magnitude) $ points
+    putStrLn $ show $ 4 * (fromIntegral count) / (fromIntegral n)
+
+main :: IO ()
+main
+  = mapM_ estimate [100,200..10000]
